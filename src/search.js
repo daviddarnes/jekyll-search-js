f66e3ba77e4fbfe67d99d75019a0ee8e6db6d9d4
David Darnes <me@daviddarnes.com>
2017-07-17 21:33:08 +0100

    Change js functions to class

--- src/search.js
+++ src/search.js
@@ -1,43 +1,51 @@
-const endpoint = '{{ "/assets/search.json" | relative_url }}';
+class jekyllSearch {
+  constructor(dataSource, searchField, resultsList) {
+    this.dataSource = dataSource;
+    this.searchField = document.querySelector(searchField);
+    this.resultsList = document.querySelector(resultsList);
 
-const pages = [];
+    this.items = this.fetchedData;
+    this.results = this.findResults;
+  }
 
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => pages.push(...data))
+  fetchedData() {
+    const items = [];
+    fetch(this.dataSource)
+      .then(blob => blob.json())
+      .then(data => items.push(...data))
+    return items;
+  }
 
-function findResults(termToMatch, pages) {
-    return pages.filter(item => {
-        const regex = new RegExp(termToMatch, 'gi');
-        return item.title.match(regex) || item.content.match(regex);
-    });
-}
+  findResults() {
+    return this.items.filter(item => {
+      const regex = new RegExp(termToMatch, 'gi');
+      return item.title.match(regex) || item.content.match(regex);
+    })
+  }
 
-function displayResults() {
-    const resultsArray = findResults(this.value, pages);
-    const html = resultsArray.map(item => {
+  displayResults() {
+    const html = this.results.map(item => {
       return `
-          <li class="item  item--result">
-              <article class="article">
-                  <h4><a href="${item.url}">${item.title}</a></h4>
-                  <p>${item.excerpt}</p>
-              </article>
-          </li>`;
+        <li class="item  item--result">
+            <article class="article">
+                <h4><a href="${item.url}">${item.title}</a></h4>
+                <p>${item.excerpt}</p>
+            </article>
+        </li>`;
     }).join('');
-    if ((resultsArray.length == 0) || (this.value == '')) {
-        resultsList.innerHTML = `<p>Sorry, nothing was found</p>`;
+    if ((this.results.length == 0) || (this.value == '')) {
+      this.resultsList.innerHTML = `<p>Sorry, nothing was found</p>`;
     } else {
-        resultsList.innerHTML = html;
+      this.resultsList.innerHTML = html;
     }
+  }
+
+  init() {
+    this.searchField.addEventListener('keyup', this.displayResults);
+    this.searchField.addEventListener('keypress', function(event) {
+        if (event.keyCode == 13) {
+          event.preventDefault();
+        }
+    });
+  }
 }
-
-const field = document.querySelector('#search');
-const resultsList = document.querySelector('#list');
-
-field.addEventListener('keyup', displayResults);
-
-field.addEventListener('keypress', function(event) {
-    if (event.keyCode == 13) {
-      event.preventDefault();
-    }
-});
